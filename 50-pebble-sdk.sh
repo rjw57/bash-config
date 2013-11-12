16b5b05c3de3f34a2d0a64fd68847068dcd4f4aa
Rich Wareham <rjw57@cam.ac.uk>
2013-11-12 11:10:51 +0000

    update location of pebble SDK

--- 50-pebble-sdk.sh
+++ 50-pebble-sdk.sh
@@ -1,3 +1,3 @@
-if [ -d "$HOME/sdk/PebbleSDK-2.0-BETA1" ]; then
-	export PATH="$HOME/sdk/PebbleSDK-2.0-BETA1/bin:$PATH"
+if [ -d "$HOME/pebble-dev/PebbleSDK-2.0-BETA1" ]; then
+	export PATH="$HOME/pebble-dev/PebbleSDK-2.0-BETA1/bin:$PATH"
 fi
