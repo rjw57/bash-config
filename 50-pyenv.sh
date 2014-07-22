8e35b3c4924b698094b95c88826960f0e075fed7
Rich Wareham <rjw57@cam.ac.uk>
2014-07-22 09:56:49 +0100

    add pyenv-virtualenv support

--- 50-pyenv.sh
+++ 50-pyenv.sh
@@ -2,6 +2,10 @@ export PYENV_ROOT="${HOME}/.pyenv"
 if [ -d "${PYENV_ROOT}" ]; then
   eval "$("${PYENV_ROOT}"/bin/pyenv init -)"
   export PATH="${PYENV_ROOT}/bin:${PATH}"
+
+  if [ -d "${PYENV_ROOT}/plugins/pyenv-virtualenv" ]; then
+      eval "$(pyenv virtualenv-init -)"
+  fi
 else
   unset PYENV_ROOT
 fi
