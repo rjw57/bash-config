a719ca9512de19c4302ee66bf374bda233ed4dfd
Rich Wareham <rjw57@cam.ac.uk>
2015-02-09 11:26:00 +0000

    add a local GOROOT under ~/sdk/go if it exists

--- 50-gopath.sh
+++ 50-gopath.sh
@@ -1,3 +1,11 @@
+# If ~/sdk/go exists, use it as the GOROOT
+if [ -d "$HOME/sdk/go" ]; then
+	export GOROOT=$HOME/sdk/go
+	if [ -d $GOROOT/bin ]; then
+		export PATH=$GOROOT/bin:$PATH
+	fi
+fi
+
 # If ~/projects/gocode exists, use it as the GOPATH
 if [ -d "$HOME/projects/gocode" ]; then
 	export GOPATH=$HOME/projects/gocode
