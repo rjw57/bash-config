6122c573f76323967a3566bcfa8adb987514d512
Rich Wareham <rjw57@cam.ac.uk>
2015-03-14 10:50:27 +0000

    npm-exec: replace with function

--- 50-npm-exec.sh
+++ 50-npm-exec.sh
@@ -1,3 +1,5 @@
-# Create npm-exec alias which runs a command including the output of "npm bin"
-# in the PATH
-alias npm-exec=PATH="$(npm bin):$PATH" "$@"
+# Create npm-exec function which runs a command including the output of "npm
+# bin" in the PATH
+function npm-exec() {
+  PATH="$(npm bin):$PATH" "$@"
+}
