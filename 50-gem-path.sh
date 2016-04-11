1649290c8cdea85227047892c74d280433273cda
Rich Wareham <rjw57@cam.ac.uk>
2016-04-11 13:26:52 +0100

    add rbbuild pluign

--- 50-gem-path.sh
+++
@@ -1,7 +0,0 @@
-if [ -d "$HOME/.gem/ruby/" ]; then
-  _gem_dirs=`find "$HOME/.gem/ruby" -maxdepth 2 -name bin -type d`
-  _gem_dir_array=($_gem_dirs)
-  if [ ! -z "${_gem_dir_array[0]}" ]; then
-    export PATH="${_gem_dir_array[0]}":$PATH
-  fi
-fi
--- 50-rbenv.sh
+++ 50-rbenv.sh
@@ -1,7 +1,7 @@
 export RBENV_ROOT="${HOME}/.rbenv"
 if [ -d "${RBENV_ROOT}" ]; then
   eval "$("${RBENV_ROOT}"/bin/rbenv init -)"
-  export PATH="${RBENV_ROOT}/bin:${PATH}"
+  export PATH="${RBENV_ROOT}/bin:${RBENV_ROOT}/plugins/ruby-build/bin:${PATH}"
 else
   unset RBENV_ROOT
 fi
