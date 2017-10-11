d4a69ac0c8006c0a9c9100c900a97ace2d7d236c
Rich Wareham <rjw57@cam.ac.uk>
2017-10-11 15:41:26 +0100

    allow use of bash completion with OSX homebrew

--- 10-bash-completion.sh
+++ 10-bash-completion.sh
@@ -2,3 +2,9 @@
 if [ -r /etc/bash_completion ]; then
 	. /etc/bash_completion
 fi
+
+if [ ! -z "`which brew`" ]; then
+  if [ -f $(brew --prefix)/etc/bash_completion ]; then
+  . $(brew --prefix)/etc/bash_completion
+  fi
+fi
