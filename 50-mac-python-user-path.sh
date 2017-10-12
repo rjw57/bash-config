e4a552147ef4bcbbd8227817106f0561bcb7eb1b
Rich Wareham <rjw57@cam.ac.uk>
2017-10-12 19:37:09 +0100

    fix mac Python path on non-mac

--- 50-mac-python-user-path.sh
+++ 50-mac-python-user-path.sh
@@ -1,3 +1,5 @@
-for bindir in `find ~/Library/Python -name bin -type d | sort -n`; do
-  export PATH=$bindir:$PATH
-done
+if [ -d ~/Library/Python ]; then
+	for bindir in `find ~/Library/Python -name bin -type d | sort -n`; do
+	  export PATH=$bindir:$PATH
+	done
+fi
