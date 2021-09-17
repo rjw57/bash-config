15ad39f81c3e3f428a64178632351c9d39b359b0
Rich Wareham <rjw57@cam.ac.uk>
2021-09-17 09:37:49 +0100

    fix mapping of keys in tmux

--- 50-tmux-conf.sh
+++ 50-tmux-conf.sh
@@ -2,5 +2,7 @@ if [ ! -f "$HOME/.tmux.conf" ]; then
   cat >"$HOME/.tmux.conf" <<EOI
 set -g default-terminal "tmux-256color"
 set -ga terminal-overrides ",xterm-256color*:Tc"
+bind-key -n Home send Escape "OH"
+bind-key -n End send Escape "OF"
 EOI
 fi
