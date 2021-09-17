d76b85bed94449e8f380e9a13903f6fe5d100eb1
Rich Wareham <rich@richwareham.com>
2021-09-17 20:39:28 +0100

    set escape delay for tmux

--- 50-tmux-conf.sh
+++ 50-tmux-conf.sh
@@ -4,5 +4,6 @@ set -g default-terminal "tmux-256color"
 set -ga terminal-overrides ",xterm-256color*:Tc"
 bind-key -n Home send Escape "OH"
 bind-key -n End send Escape "OF"
+set -s escape-time 25
 EOI
 fi
