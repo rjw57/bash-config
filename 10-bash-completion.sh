e05785ee0108f52334b2cb3fc7b6de40c1f429b1
Rich Wareham <rich@richwareham.com>
2022-11-20 13:41:52 +0000

    bash-completion: squash error message from which

--- 10-bash-completion.sh
+++ 10-bash-completion.sh
@@ -3,7 +3,7 @@ if [ -r /etc/bash_completion ]; then
 	. /etc/bash_completion
 fi
 
-if [ ! -z "`which brew`" ]; then
+if [ ! -z "`which brew 2>/dev/null`" ]; then
   if [ -f $(brew --prefix)/etc/bash_completion ]; then
   . $(brew --prefix)/etc/bash_completion
   fi
