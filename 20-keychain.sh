07456e6141052af51b80b410040458d46b8c83e4
Rich Wareham <rich@richwareham.com>
2026-05-25 12:48:53 +0100

    feat: silence keychain which warning if not present

--- 20-keychain.sh
+++ 20-keychain.sh
@@ -1,4 +1,4 @@
 # Keychain allows sharing a single ssh-agent between logins
-if [ ! -z "`which keychain`" ]; then
+if [ ! -z "$(which keychain 2>/dev/null)" ]; then
   eval `keychain --eval --quiet`
 fi
